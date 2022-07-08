85dcfe1ff21d3c08029ae679e501cde887a80a80
ruslan <rus.magamedov.99@bk.ru>
2022-07-08 10:27:54 +0300

    created controllers

---
+++ controllers/main.js
@@ -0,0 +1,33 @@
+const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
+const { BadRequestError } = require("../errors");
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  // just for demo, normally provided by DB!!!
+  const id = new Date().getTime();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(StatusCodes.OK).json({ msg: "user created", token });
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+
+  res.status(StatusCodes.OK).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
+};
+
+module.exports = {
+  login,
+  dashboard,
+};
